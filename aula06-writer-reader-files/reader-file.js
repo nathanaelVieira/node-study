13d9b7d6ad883bca260047d8bfde6e5cf9e652bf
Nathanael Vieira <vieira_trabalho@hotmail.com>
2024-03-03 17:00:01 -0300

    aula06-writer-file: Manuseo JSON IO

--- aula06-writer-reader-files/reader-file.js
+++ aula06-writer-reader-files/reader-file.js
@@ -1,34 +1,25 @@
 const fileReader = require('fs');
 const path = require('path');
 
+const pathFile = path.resolve(__dirname, '..', 'test.json');
 
-const pathFile = path.resolve(__dirname, '..', 'test.txt');
+const people = [
+    { message: 'text/txt -> file system writer. 💚 learning node.js\n\tauthor: Nathanael L. Vieira' },
+    { nome: 'João' },
+    { nome: 'Marcia' },
+    { nome: 'Valmir' },
+    { nome: 'Cleber' },
+    { nome: 'Gabriel' },
+    { nome: 'Beatriz dona encrenca 💌' }
+];
+
+const peopleJSON = JSON.stringify(people, '', 4); // 4 -> valor para identação
 
 
-const content = 'text/txt -> file system writer. 💚 learning node.js\n\tauthor: Nathanael L. Vieira';
-
-
-// fileReader.writeFile(file, data, options, callback)
-/*
-    file -> caminho/ diretório.
-    data -> dados a serem escritos no arquivo.
-    option -> {
-        flag -> {
-            'w' -> sobreescrita
-            'a' -> adiciona/ concatena ao final
-            }
-        enconding -> padrão de caracateres [padrão utf-8]
-    }.
-
-    callback -> função chama ao terminar processo. Famoso callback.
-*/
-
-fileReader.writeFile(pathFile, content, {
+fileReader.writeFile(pathFile, peopleJSON, {
     flag: 'w',
     encoding: 'utf-8'
 }, (err) => {
     if (err) throw err;
     console.log("File created with successfully");
 });
-
-
